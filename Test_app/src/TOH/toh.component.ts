d395fbee4e7ebe57032a96ee743fec853b59570e
Shrey-shu <84721344+Shrey-shu@users.noreply.github.com>
2022-10-30 21:46:32 +0530

    Delete Test_app directory

--- Test_app/src/TOH/toh.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from "@angular/core";
-@Component({
-    selector:'toh',
-    templateUrl:'./toh.html',
-    styleUrls: ['./toh.scss']
-})
-export class TOH{
-    name = 'Tour of Heros';
-}
